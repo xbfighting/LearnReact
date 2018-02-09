35b1c5c464fc8311331b706d4acba9f1743166db
baileicheng <xbfighting@hotmail.com>
2018-02-09 11:30:36 +0800

    [mod] es6 class  construct bind event

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -1,21 +1,79 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
+const list = [
+  {
+    title: 'React.',
+    url: 'https://facebook.github.io/react/',
+    author: 'Jordan Walke.',
+    num_comments: 3,
+    points: 4,
+    objectID: 0
+  }, {
+    title: 'Redux',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1
+  }
+];
+
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list
+    };
+
+    // React 的官方文档中坚持在构造函数中绑定类方法
+    this.onDismiss = this
+      .onDismiss
+      .bind(this);
+  }
+
+  onClickMe = (obj) => {
+    console.log("onClickMe" + obj);
+  }
+
+  onDismiss(id) {
+    const updateList = this
+      .state
+      .list
+      .filter(item => item.objectID !== id);
+    this.setState({list: updateList});
+  }
+
   render() {
+    const helloWorld = "Welcome to the Road to learn React";
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-          Hello World!
-        <p>
-        </p>
+        {this
+          .state
+          .list
+          .map(item => {
+            return (
+              <div key={item.objectID}>
+                <span>
+                  <a href={item.url}>{item.title}</a>
+                </span>
+                <span>{item.author}</span>
+                <span>{item.num_comments}</span>
+                <span>{item.points}</span>
+                <span>
+                  <button onClick= {()=> this.onDismiss(item.objectID)} type="button">
+                    Dismiss
+                  </button>
+                  < button onClick= {() => this.onClickMe(item.objectID)} type="button">
+                    ClickMe
+                  </button>
+                </span>
+              </div>
+            );
+          })
+}
       </div>
     );
   }
