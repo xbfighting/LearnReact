d10228d4c9198fff7e55f39ffe4c4695bdab28bb
baileicheng <xbfighting@hotmail.com>
2018-02-09 16:49:59 +0800

    [mod] 类转化为函数式无状态组件

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -75,52 +75,48 @@ class App extends Component {
   }
 }
 
-class Search extends Component {
-  render() {
-    const {value, onChange, children} = this.props;
-    return (
-      <form>
-        {children}
-        <input type="text" value={value} onChange={onChange}/>
-      </form>
-    );
-  }
+const Search = ({value, onChange, children}) => {
+  return (
+    <form>
+      {children}
+      <input type="text" value={value} onChange={onChange}/>
+    </form>
+  );
 }
 
-class Table extends Component {
-  render() {
-    const {list, pattern, onDismiss} = this.props;
-    return (
-      <div>
-        {list
-          .filter(isSearched(pattern))
-          .map(item => <div key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-            <span>
-              < Button onClick= { () => onDismiss(item.objectID) }>
-                Dismiss
-              </Button>
-            </span>
-          </div>)}
-      </div>
-    );
-  }
+const Table = ({list, pattern, onDismiss}) => {
+  return (
+    <div>
+      {list
+        .filter(isSearched(pattern))
+        .map(item => <div key={item.objectID}>
+          <span>
+            <a href={item.url}>{item.title}</a>
+          </span>
+          <span>{item.author}</span>
+          <span>{item.num_comments}</span>
+          <span>{item.points}</span>
+          <span>
+            < Button onClick= { () => onDismiss(item.objectID) }>
+              Dismiss
+            </Button>
+          </span>
+        </div>)}
+    </div>
+  );
 }
 
-class Button extends Component {
-  render() {
-    const {onClick, className='', children} = this.props;
-    return (
-      <button className={className} onClick={onClick} type="button">
-        {children}
-      </button>
-    );
-  }
+const Button = ({
+  onClick,
+  className = '',
+  children
+}) => {
+  // do st
+  return (
+    <button className={className} onClick={onClick} type="button">
+      {children}
+    </button>
+  );
 }
 
 export default App;
